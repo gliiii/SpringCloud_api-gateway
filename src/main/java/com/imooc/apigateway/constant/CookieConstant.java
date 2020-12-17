2f9365f3977b5a8525831d43c91392811340b170
ligen <ligen3@yusys.com.cn>
2020-12-17 22:01:47 +0800

    9-7 完成权限校验(上)

---
+++ src/main/java/com/imooc/apigateway/constant/CookieConstant.java
@@ -0,0 +1,13 @@
+package com.imooc.apigateway.constant;
+
+public interface CookieConstant {
+
+    String TOKEN = "token";
+
+    String OPENID = "openId";
+
+    /**
+     * 过期时间（单位：s）
+     */
+    Integer EXPIRE = 7200;
+}
